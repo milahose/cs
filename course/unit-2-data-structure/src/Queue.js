2165dd1f45c64b403a12f53d1ab2f4d75b8c13e3
Mila Hose <mila.hose@gmail.com>
2017-07-30 10:30:38 -0700

    Add 'queue' solution

--- course/unit-2-data-structure/src/Queue.js
+++ course/unit-2-data-structure/src/Queue.js
@@ -1,12 +1,24 @@
 // do not use any native array methods
 function Queue() {
-
+	this.storage = {};
+	this.index = 0; 
+	this.counter = 0;
 }
 
 Queue.prototype.enqueue = function(value) {
-
+	this.storage[this.index] = value;
+	this.index++;
 };
 
-Queue.prototype.dequeue = function() {
+Queue.prototype.dequeue = function(value) {
+	var deletedValue;
 
+	if (this.counter === this.index) {
+		return undefined;
+	}
+
+	deletedValue = this.storage[this.counter];
+	delete this.storage[this.counter];
+	this.counter++;
+	return deletedValue;
 };
