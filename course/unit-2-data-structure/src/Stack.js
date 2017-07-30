aa5bebdefcd7ad242f913d1dc1afe5f79ee3de39
Mila Hose <mila.hose@gmail.com>
2017-07-30 10:27:45 -0700

    Add 'stack' solution

--- course/unit-2-data-structure/src/Stack.js
+++ course/unit-2-data-structure/src/Stack.js
@@ -5,9 +5,19 @@ function Stack() {
 }
 
 Stack.prototype.push = function(value) {
-
+	this.storage[this.index] = value;
+	this.index++;
 };
 
 Stack.prototype.pop = function() {
+	var deletedValue; 
 
+	if (this.index === 0) {
+		return undefined;
+	}
+
+	this.index--;
+	deletedValue = this.storage[this.index];
+	delete this.storage[this.index];
+	return deletedValue;
 };
