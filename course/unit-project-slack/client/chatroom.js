7e0fffe1a00d8d3b1bb1fb639cdd0eaaca6e5f38
Mila Hose <mila.hose@gmail.com>
2017-08-03 17:34:45 -0700

    Add slack updates

---
+++ course/unit-project-slack/client/chatroom.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    $('#submit').on('click', postMessage);
+    let previousMessage;
+    let storage;
+    let temp = [];
+
+    function postMessage () {
+        let postBody = {
+            created_by: 'Jon and Mila',
+            message: $('#messageText').val(),
+        };
+        $.ajax({
+            type: 'POST',
+            url: 'http://slack-server.elasticbeanstalk.com/messages',
+            contentType: 'application/json',
+            data: JSON.stringify(postBody)
+        })
+    };
+
+    function refreshFeed () {
+        $.ajax({
+            type: 'GET',
+            url: 'http://slack-server.elasticbeanstalk.com/messages',
+            success: function (data) {
+                for (let i = 0; i < data.length; i++) {
+                    if (storage[0].created_at === data[i].created_at || storage[0].message === data[i].message) {
+                        break;
+                    }
+                    temp.push(data[i]);
+                }
+                storage = data;
+                temp.forEach((x, i) => {
+                    let userName = $('<div class="username">' + x.created_by + '</div>');
+                    let message = $('<div class="message">' + x.message + '</div>');
+                    $('#chatbox')
+                        .append(userName)
+                        .append(message);
+                });
+                temp = [];
+                setTimeout(refreshFeed, 500);
+            }
+        });
+        
+    };
+
+    
+    let request = new XMLHttpRequest();
+    request.open('GET', 'http://slack-server.elasticbeanstalk.com/messages', true);
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            let data = JSON.parse(request.response);
+            storage = data;
+            for (let i = data.length - 1; i > 0; i--) {
+                let userName = $('<div class="username">' + data[i].created_by + '</div>');
+                let message = $('<div class="message">' + data[i].message + '</div>');
+                $('#chatbox')
+                    .append(userName)
+                    .append(message);
+                previousMessage = data[0];
+            }
+        } else {
+            console.log('error');
+        }
+    };
+    request.send();
+
+    setTimeout(refreshFeed, 1000);
+
+});
--- course/unit-project-slack/client/main.js
+++ course/unit-project-slack/client/main.js
@@ -1,75 +1,57 @@
-// The following code appends a title to the page
-// document.createElement creates an element that can be altered and then inserted into the DOM
-// document.body.appendChild places a node as a child under the body element
-// var title = document.createElement('div');
-// title.innerHTML = 'Social Calendar';
-// document.body.appendChild(title);
+class Calendar {
+  //do something with the data here
+  constructor(schedule) {
+
+  }
+}
+
+class Event {
+  constructor(data) {
+    this.startTime = data.startTime;
+    this.endTime = data.endTime;
+    this.description = data.description;
+  }
+}
+
+class Day {
+  constructor(name) {
+    this.name = name;
+    this.events = [];
+    this.addEvent = (eventData) => {
+      this.events.push(new Event(eventData));
+    }
+    this.orderEvents = () => {
+      //go through events and sort them by start and end time
+    }
+  }
+}
+
+class Week {
+  constructor(number) {
+    this.days = [];
+    this.addDay = (name) => {
+      const day = new Day(name);
+      this.days.push(day);
+    }
+  }
+}
 
 
-
-// Your schedule can be accessed through the global object "schedule"
-// console.log(schedule);
-
-
-
-
-// Slack Clone 
-$(document).ready(function() {
-    $('#submit').on('click', postMessage);
-    let previousMessage;
-
-    function postMessage () {
-        let postBody = {
-            created_by: 'Jon and Mila',
-            message: $('#messageText').val(),
-        };
-        $.ajax({
-            type: 'POST',
-            url: 'http://slack-server.elasticbeanstalk.com/messages',
-            contentType: 'application/json',
-            data: JSON.stringify(postBody)
-        })
-    };
-
-    function refreshFeed () {
-        $.ajax({
-            type: 'GET',
-            url: 'http://slack-server.elasticbeanstalk.com/messages',
-            success: function (data) {
-                if (data[0].created_by !== previousMessage.created_by || data[0].message !== previousMessage.message) {
-                    let userName = $('<div class="username">' + data[0].created_by + '</div>');
-                    let message = $('<div class="message">' + data[0].message + '</div>');
-                    $('#chatbox')
-                        .append(userName)
-                        .append(message);
-                    previousMessage = data[0];
-                }
-                setTimeout(refreshFeed, 500);
-            }
-        });
-        
-    };
-
-    
-    let request = new XMLHttpRequest();
-    request.open('GET', 'http://slack-server.elasticbeanstalk.com/messages', true);
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-            let data = JSON.parse(request.response);
-            for (let i = data.length - 1; i > 0; i--) {
-                let userName = $('<div class="username">' + data[i].created_by + '</div>');
-                let message = $('<div class="message">' + data[i].message + '</div>');
-                $('#chatbox')
-                    .append(userName)
-                    .append(message);
-                previousMessage = data[0];
-            }
-        } else {
-            console.log('error');
-        }
-    };
-    request.send();
-
-    refreshFeed();
-
+$(document).on('ready', () => {
+  const title = $('<h1>').text('Social Calendar');
+  $('body').children().append(title);
+  console.log(title);
+  
+  // Make an AJAX request to the server, then do something with the result!
+  $.ajax({
+    type: 'GET',
+    url: 'http://slack-server.elasticbeanstalk.com/calendar/16',
+    success: function(data) {
+      console.log(data);
+      for (let prop in data) {
+        $('body').append(prop);
+      }
+    }
+  })
+  
 });
