57ced8421c3a67fa1394edc3cf0a3ba5ee9564a9
Mila Hose <mila.hose@gmail.com>
2017-08-07 11:33:20 -0700

    Add angular unit

---
+++ course/unit-7-angular/client/app.js
@@ -0,0 +1,16 @@
+const app = angular
+  .module('myApp', [
+    'ngRoute',
+    'Codesmith.HomeController',
+  ]);
+
+app.config(configFunction);
+
+function configFunction($routeProvider, $locationProvider) {
+
+  $routeProvider
+    .when('/', {
+      templateUrl: './partials/home.html',
+      controller: 'HomeController'
+    });
+}
