3771fbd8eda2c2d30f5adb56361989db25183062
Mila Hose <mila.hose@gmail.com>
2017-08-09 11:02:11 -0700

    Add partial solution

--- course/hack-hour-16/challenges/roman-numeral.js
+++ course/hack-hour-16/challenges/roman-numeral.js
@@ -18,6 +18,32 @@
  */
 
 function romanNumeral(n) {
+	var obj = {
+    "1000": "M",
+    "900": "CM",
+    "500": "D",
+    "400": "CD",
+    "100": "C",
+    "90": "XC",
+    "50": "L",
+    "40": "XL",
+    "10": "X",
+    "9": "IX",
+    "5": "IX",
+    "4": "IV", 
+    "1": "I",
+	};
+
+	var nStr = String(n);
+	var count = nStr.length;
+	var tens = Math.floor(n / 10);
+	var fives = Math.floor((n - (tens * 10)) / 5);
+	var ones = n - ((tens * 10) + (fives * 5));
+	var romanNumeral = "";
+
+	for (var prop in obj) {
+		if ()
+	}
 
 }
 
