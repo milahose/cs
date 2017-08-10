3bc585997013ddd5b5bdc969a4eb1805a8b79524
Mila Hose <mila.hose@gmail.com>
2017-08-10 09:49:47 -0700

    Add node pages attempt

--- course/unit-8SB-node-pages/server/server.js
+++ course/unit-8SB-node-pages/server/server.js
@@ -2,5 +2,9 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((request, response) => {
-
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write(html);
+	response.end()
 }).listen(3000);
+
+
