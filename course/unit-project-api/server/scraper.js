d1e2357282cdd1d24baa8b2347e8f5f9ceebe227
Mila Hose <mila.hose@gmail.com>
2017-08-11 20:41:02 -0700

    Add scraper code

--- course/unit-project-api/server/scraper.js
+++ course/unit-project-api/server/scraper.js
@@ -4,15 +4,18 @@ const cheerio = require('cheerio');
 const request = require('request');
 
 const scrapeController = {
-  getData: (req, res, next) => {
+    getData: (req, res, next) => {
 
-    // change URL to any site that you want
-    request('http://www.producthunt.com/', (error, response, html) => {
-      let $ = cheerio.load(html);
-      // add code here
+        // change URL to any site that you want
+        request('http://www.producthunt.com/', (error, response, html) => {
+            let $ = cheerio.load(html);
+            // add code here
 
-    });
-  }
+        });
+
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ a: 1 }));
+    }
 };
 
 module.exports = scrapeController;
--- course/unit-project-api/server/server.js
+++ course/unit-project-api/server/server.js
@@ -2,10 +2,19 @@
 
 const express = require('express');
 const app = express();
+const router = express.Router();
 const scraperController = require('./scraper');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // first sample route
-app.get('/sample', scraperController.getData);
+app.get('/', scraperController.getData);
+
+app.get('/example', scraperController.getData);
 
 app.listen(3000);
 
