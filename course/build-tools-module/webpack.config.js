e0d698213917384c00e1e43eff27b9c32f65ede8
Mila Hose <mila.hose@gmail.com>
2017-08-21 16:48:56 -0700

    Add progress

--- course/build-tools-module/webpack.config.js
+++ course/build-tools-module/webpack.config.js
@@ -1,9 +1,24 @@
+const path = require('path');
+
 const config = {
 	entry: './client/index.js',
 	output: {
+		path: path.join(__dirname, '/build'),
+		publicPath: "/build/",
     filename: 'webpack-bundle.js',
-    path: './bundle'
-  }
+  }, 
+  module: {
+    loaders: [
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        }
+      },
+      { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
+    ],
+  },
 }
 
 module.exports = config; 
