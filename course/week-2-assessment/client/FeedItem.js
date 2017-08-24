d93341eefb2642ba7ead826f03b704bf36619158
Mila Hose <mila.hose@gmail.com>
2017-08-24 09:34:23 -0700

    Add solo project updates

--- course/week-2-assessment/client/FeedItem.js
+++ course/week-2-assessment/client/FeedItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 // FeedItem should consist of an image (src contained in the data from the AJAX request)
 class FeedItem extends Component {
