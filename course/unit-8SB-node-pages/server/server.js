d52ac78179dc149876594b5fc09b904b3d1d5201
Mila Hose <mila.hose@gmail.com>
2017-09-04 19:50:49 -0700

    Add server code

--- course/unit-8SB-node-pages/server/server.js
+++ course/unit-8SB-node-pages/server/server.js
@@ -1,10 +1,16 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((request, response) => {
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write(html);
-	response.end()
-}).listen(3000);
+var server = http.createServer(function(reques, response) {
+  fs.readFile(__dirname + '/index.html', function(err, data) {
+    if (err) throw err;
 
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    response.end();
+  });
+}); 
 
+server.listen(3000, function() {
+  console.log("Listening on port 3000");
+});
