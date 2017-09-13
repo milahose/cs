07b8accc4d8025a9d279611a3acef4d1830e5377
Mila Hose <mila.hose@gmail.com>
2017-09-13 09:18:46 -0700

    Add linked list

--- course/hack-hour-16/challenges/count-twos.js
+++ course/hack-hour-16/challenges/count-twos.js
@@ -1,4 +1,6 @@
-// Write a function "countTwos" that takes a number as an argument, n. The function will return the number of 2s encountered when counting from 1 to n (inclusive). 2222 counts as having 4 2s.
+// Write a function "countTwos" that takes a number as an argument, n. 
+// The function will return the number of 2s encountered when counting from 1 to n (inclusive). 
+// 2222 counts as having 4 2s.
 
 // countTwos(1);  -> 0
 // countTwos(3);  -> 1
